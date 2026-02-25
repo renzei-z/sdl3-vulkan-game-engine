9dacd4a505d030ce94bc16d45843832ae929881c
renzei-z <renzei_z@protonmail.com>
2026-02-25 15:04:09 +0900

    make triangle spin, and add some color constants.

---
+++ include/engine/colors.h
@@ -0,0 +1,8 @@
+#ifndef ENGINE_COLORS_H_
+#define ENGINE_COLORS_H_
+
+#define COLOR_RED   {1.0f, 0.0f, 0.0f}
+#define COLOR_GREEN {0.0f, 1.0f, 0.0f}
+#define COLOR_BLUE  {0.0f, 0.0f, 1.0f}
+
+#endif // ENGINE_COLORS_H_
--- src/main.c
+++ src/main.c
@@ -3,11 +3,20 @@
 #endif // __VK_BACKEND
 
 #include <engine.h>
+#include <engine/colors.h>
+
 #include <platform.h>
 
 #include <math.h>
-
 #include <SDL3/SDL.h>
+
+void temp_rotate(vertex *v, float angle) {
+  float x_old = v->pos[0];
+  float y_old = v->pos[1];
+  
+  v->pos[0] = x_old * cosf(angle) - y_old * sinf(angle);
+  v->pos[1] = x_old * sinf(angle) + y_old * cosf(angle); 
+}
 
 int main(void) {
     engine_state engine = {0};
@@ -31,11 +40,21 @@ int main(void) {
 
 	engine_begin_frame(&engine);
 
-	float t = SDL_GetTicks() / 1000.0f;
-	vertex v1 = {{ 0.0f, -0.5f + sinf(t*3) * 0.2f, 0.0f }, {1,0,0}, {0.5,0}};
-	vertex v2 = {{ 0.2f - 0.3f * cosf(t*2),  0.5f, 0.0f }, {0,1,0}, {1,1}};
-	vertex v3 = {{-0.5f,  0.5f - 0.4f*sinf(t/2), 0.0f }, {0,0,1}, {0,1}};
+	float time = SDL_GetTicks() / 1000.0f;
+	float total_angle = time * 2.0f;
 
+	float size = 0.3f;
+	
+	vertex v1 = {{ 0.0f, -size, 0.0f }, {1,0,0}, {0.5,0}};
+	vertex v2 = {{ size,  size, 0.0f }, {0,1,0}, {1,1}};
+	vertex v3 = {{-size,  size, 0.0f }, {0,0,1}, {0,1}};
+
+	temp_rotate(&v1, total_angle);
+	temp_rotate(&v2, total_angle);
+	temp_rotate(&v3, total_angle);
+
+	engine_draw_triangle(&engine, v1, v2, v3);	
+	
 	engine_draw_triangle(&engine, v1, v2, v3);
 	
 	engine_do_render(&engine);
