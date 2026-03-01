f69ceb1125a8d4727a6ba93527abba50c38e705f
renzei-z <renzei_z@protonmail.com>
2026-03-01 17:21:39 +0900

    feat: remove alignment stuff in emm for now.

--- libs/emm/include/emm.h
+++ libs/emm/include/emm.h
@@ -21,10 +21,9 @@ typedef union emm_vec2_t {
 } emm_vec2;
 
 typedef union emm_vec3_t {
-  // _unused_w/a is included to ensure that emm_vec3 is aligned to 16-bytes.
-  struct { float x, y, z, _unused_w; };
-  struct { float r, g, b, _unused_a; };
-  float raw[4];
+  struct { float x, y, z; };
+  struct { float r, g, b; };
+  float raw[3];
 } emm_vec3;
 
 typedef union emm_vec4_t {
@@ -38,15 +37,8 @@ typedef union emm_mat2_t {
   emm_vec2 columns[2];
 } emm_mat2;
 
-// NOTE: We use 12 elements for raw here as, because of padding, the structure
-// in memory is:
-// [0] [1] [2] [PAD]
-// [3] [4] [5] [PAD]
-// [6] [7] [8] [PAD]
-// and only using 9 would cut off after [6].
-
 typedef union emm_mat3_t {
-  float raw[12];
+  float raw[9];
   emm_vec3 columns[3];
 } emm_mat3;
 
@@ -66,19 +58,6 @@ typedef emm_mat3 mat3;
 typedef emm_mat4 mat4;
 
 #endif // EMM_NO_STRIP_PREFIXES
-
-#ifdef EMM_ASSERT_ALIGNMENTS
-#define emm_check_align(type, size, name) static_assert(sizeof(type) == size, "Alignment error: "name". This is being treated as an error, as EMM_ASSERT_ALIGNMENTS is defined.")
-
-emm_check_align(emm_vec2,  8, "emm_vec2");
-emm_check_align(emm_vec3, 16, "emm_vec3");
-emm_check_align(emm_vec4, 16, "emm_vec4");
-
-emm_check_align(emm_mat2, 16, "emm_mat2");
-emm_check_align(emm_mat3, 48, "emm_mat3");
-emm_check_align(emm_mat4, 64, "emm_mat4");
-
-#endif // EMM_ASSERT_ALIGNMENTS
 
 HEADER_END
 
